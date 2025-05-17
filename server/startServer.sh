13d99be8f436dedd467b7abc645a35f904565e22
msp <msp@netgroup.su>
2025-05-18 00:50:58 +0300

    correction code

--- server/startServer.sh
+++ server/startServer.sh
@@ -1,16 +1,33 @@
 #!/usr/bin/env bash
 
-# Проверяем, установлен ли Python 3
-if ! command -v python3 &> /dev/null; then
-    echo "Python 3 не найден. Установите его и повторите попытку."
-    exit 1
-fi
+# Функция для проверки и установки Python
+install_or_update_python() {
+    if ! command -v python3 &> /dev/null; then
+        echo "Python 3 не найден. Установка Python 3..."
+        apt update && apt install -y python3
+    else
+        echo "Python 3 уже установлен. Проверка обновлений..."
+        apt-get upgrade -y python3
+    fi
+}
 
-# Проверяем, установлен ли pip
-if ! command -v pip3 &> /dev/null; then
-    echo "pip3 не найден. Установите pip и повторите попытку."
-    exit 1
-fi
+# Функция для проверки и установки pip
+install_or_update_pip() {
+    if ! command -v pip3 &> /dev/null; then
+        echo "pip3 не найден. Установка pip..."
+        apt install -y python3-pip
+    else
+        echo "pip3 уже установлен. Обновление pip..."
+        pip3 install --upgrade pip
+    fi
+}
+
+
+# Установка или обновление Python 3
+install_or_update_python
+
+# Установка или обновление pip
+install_or_update_pip
 
 # Создаем виртуальное окружение
 echo "Создание виртуального окружения..."
@@ -19,7 +36,7 @@ source venv/bin/activate
 
 # Устанавливаем зависимости из requirements.txt
 echo "Установка зависимостей из requirements.txt..."
-pip3 install -r requirements.txt
+pip3 install -r /opt/requirements.txt
 
 # Запускаем сервер
 echo "Запуск QUIC сервера..."
