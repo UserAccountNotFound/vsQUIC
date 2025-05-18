29c02ca2d0f618556a86af2662784a6020aa5c5f
msp <msp@netgroup.su>
2025-05-19 01:58:16 +0300

    modified:   client/startClient.sh 	modified:   server/startServer.sh

--- client/startClient.sh
+++ client/startClient.sh
@@ -29,16 +29,18 @@ install_or_update_pip() {
 
 # Функция для создания виртуального окружения
 init_venv() {
+    local venv_path="/opt/venv"
     echo "Проверка виртуального окружения..."
-    if [ ! -d "venv" ]; then
-        python3 -m venv venv
-        echo "Виртуальное окружение создано"
+    
+    if [ ! -d "$venv_path" ]; then
+        python3 -m venv "$venv_path"
+        echo "Виртуальное окружение создано в $venv_path"
     else
-        echo "Виртуальное окружение уже существует"
+        echo "Виртуальное окружение уже существует в $venv_path"
     fi
     
     echo "Активация виртуального окружения"
-    source venv/bin/activate
+    source "$venv_path/bin/activate"
 }
 
 # Проверка, была ли уже выполнена установка системных пакетов
